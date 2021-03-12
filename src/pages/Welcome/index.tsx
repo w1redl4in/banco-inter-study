ececfac8264acdf3c93e01dd31d0d6c64c0cc1a2
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-12 17:17:10 -0300

    :zap: feat: welcome page

--- src/pages/Welcome/index.tsx
+++ src/pages/Welcome/index.tsx
@@ -1,7 +1,46 @@
-import { WelcomeContainer } from './styles';
+import { AnimatedContainer, Form, SignInButton } from './styles';
+import AuthLayout from '../_layouts/auth';
+import { BancoInter } from '../../assets/images';
+import { useCallback, useState } from 'react';
 
 const Welcome: React.FC = () => {
-  return <WelcomeContainer>Welcome</WelcomeContainer>;
+  const [userName, setUserName] = useState('');
+
+  const handleUserName = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>): void => {
+      setUserName(event.target.value);
+    },
+    []
+  );
+
+  console.log('userName', userName);
+
+  return (
+    <AuthLayout>
+      <AnimatedContainer>
+        <h1>
+          <BancoInter />
+        </h1>
+        <h2>
+          Estudo feito por @lainwired em cima do redesign do @luke_morales.
+        </h2>
+        <Form>
+          <label htmlFor="name">
+            <input
+              type="text"
+              name="name"
+              placeholder="Digite seu nome para entrar"
+              value={userName}
+              onChange={handleUserName}
+            />
+          </label>
+          <SignInButton type="button" disabled={!userName}>
+            Entrar
+          </SignInButton>
+        </Form>
+      </AnimatedContainer>
+    </AuthLayout>
+  );
 };
 
 export default Welcome;
