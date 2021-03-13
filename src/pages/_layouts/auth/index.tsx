3cd0c3f18f28e75fb3cd937ccb963959ee357b7a
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-13 16:15:10 -0300

    :sparkles: feat: auth layout

--- src/pages/_layouts/auth/index.tsx
+++ src/pages/_layouts/auth/index.tsx
@@ -1,8 +1,25 @@
 import { PropsWithChildren } from 'react';
+import { useTheme } from 'styled-components';
 import { AnimatedContainer } from './styles';
 
-const AuthLayout: React.FC = ({ children }: PropsWithChildren<unknown>) => {
-  return <AnimatedContainer>{children}</AnimatedContainer>;
+export type AuthLayoutProps = {
+  background: string;
+};
+
+const AuthLayout = (props: PropsWithChildren<AuthLayoutProps>) => {
+  const { children, background } = props;
+  const { colors } = useTheme();
+
+  return (
+    <AnimatedContainer
+      initial={false}
+      transition={{ duration: 0.35 }}
+      animate={{ background }}
+      exit={{ background: colors.lightGrey }}
+    >
+      {children}
+    </AnimatedContainer>
+  );
 };
 
 export default AuthLayout;
