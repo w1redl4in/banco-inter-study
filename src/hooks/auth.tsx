6c149c9111c5a42be8be55ed289c1dfc83c1d9f6
Felipe <felipe.araujo@hubfintech.com.br>
2021-03-13 16:31:15 -0300

    :pushpin: feat: added signed state and signOut function in auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -13,6 +13,8 @@ export type User = string;
 export type AuthContext = {
   signIn(name: string): void;
   currentUser: User;
+  signed: boolean;
+  signOut(): void;
 };
 
 const AuthContext = createContext<AuthContext | null>(null);
@@ -21,17 +23,26 @@ export const AuthContextProvider = ({
   children,
 }: PropsWithChildren<unknown>) => {
   const [currentUser, setCurrentUser] = useState<User>('');
+  const [signed, setSigned] = useState(false);
 
   const signIn = useCallback((name: string) => {
     setCurrentUser(name);
+    setSigned((sign) => !sign);
+  }, []);
+
+  const signOut = useCallback(() => {
+    setCurrentUser('');
+    setSigned((sign) => !sign);
   }, []);
 
   const value = useMemo(
     () => ({
       currentUser,
       signIn,
+      signed,
+      signOut,
     }),
-    [currentUser, signIn]
+    [currentUser, signIn, signed, signOut]
   );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
